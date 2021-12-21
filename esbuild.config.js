5b4fa2f93eeb374cfbd4c75c7153b4ec925bc2a7
Joe Workman <joe@workmanmail.com>
2021-12-21 11:16:57 -0800

    Sass + JS build

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,37 +1,48 @@
-const esbuild = require('esbuild');
-const glob    = require("tiny-glob");
-const copy    = require('esbuild-plugin-copy');
-const { sassPlugin }   = require("esbuild-sass-plugin");
-const {createImporter} = require("sass-extended-importer");
+const copy               = require('esbuild-plugin-copy');
+// const esBuildDevServer   = require("esbuild-dev-server")
+const esbuild            = require('esbuild');
+const { globPlugin }     = require('esbuild-plugin-glob');
+const { sassPlugin }     = require("esbuild-sass-plugin");
+const { createImporter } = require("sass-extended-importer");
+// const { exec }           = require('child_process');
 
-(async () => {
+esbuild.build({
+    entryPoints : [
+        "src/js/*.js",
+        "src/styles/[a-zA-Z]*.scss",
+    ],
+    bundle      : true,
+    minify      : false,
+    // metafile    : true,
+    // watch       : true,
+    // incremental : true,
+    outdir      : 'dist/assets/',
+    external    : ["jquery", "what-input"],
+    plugins     : [
+        globPlugin(),
+        // Sass includes
+        sassPlugin({
+            includePaths: [
+                "node_modules/foundation-sites/scss/",
+                "node_modules/motion-ui/src/",
+            ],
+            importer: createImporter(),
+        }),
+        // Copy in the static external libraries
+        copy.default({assets: {
+            from: [
+                "node_modules/jquery/dist/jquery.min.js",
+                "node_modules/what-input/dist/what-input.min.js",
+            ],
+            to: [ "js/vendor" ]
+        }})
+    ],
+});
 
-    const files = await glob("src/{js,styles}/[a-zA-Z]*.*");
-    await esbuild.build({
-        entryPoints : files,
-        bundle      : true,
-        minify      : false,
-        watch       : false,
-        outdir      : 'dist/assets/',
-        external    : ["jquery", "what-input"],
-        plugins     : [
-            // Sass includes
-            sassPlugin({
-                includePaths: [
-                    "node_modules/foundation-sites/scss/",
-                    "node_modules/motion-ui/src/",
-                ],
-                importer: createImporter(),
-            }),
-            // Copy in the static external libraries
-            copy.default({assets: {
-                from: [
-                    "node_modules/jquery/dist/jquery.min.js",
-                    "node_modules/what-input/dist/what-input.min.js",
-                ],
-                to: [ "js" ]
-            }})
-        ],
-    }).catch(() => process.exit(1));
-
-})();
+// esBuildDevServer.start(, {
+//     port            : "8080",
+//     watchDir        : "src",
+//     index           : "dist/index.html",
+//     staticDir       : "dist",
+//     onBeforeRebuild : () => console.log("onBeforeRebuild"),
+// });
