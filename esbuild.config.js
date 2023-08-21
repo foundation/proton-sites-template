757db8cda18daecd30c21bbefc7c6be5917568eb
Joe Workman <joe@workmanmail.com>
2023-08-21 07:16:26 -0700

    update build tools to latest versions

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,5 +1,5 @@
-const copy               = require('esbuild-plugin-copy');
 const esbuild            = require('esbuild');
+const { copy }           = require('esbuild-plugin-copy');
 const { globPlugin }     = require('esbuild-plugin-glob');
 const { sassPlugin }     = require("esbuild-sass-plugin");
 const { createImporter } = require("sass-extended-importer");
@@ -9,8 +9,9 @@ esbuild.build({
         "src/js/*.js",
         "src/styles/[a-zA-Z]*.scss",
     ],
-    bundle      : true,
-    minify      : false,
+    bundle    : true,
+    minify    : false,
+    // sourcemap : true,
     // metafile    : true,
     // watch       : true,
     // incremental : true,
@@ -20,19 +21,21 @@ esbuild.build({
         globPlugin(),
         // Sass includes
         sassPlugin({
-            includePaths: [
+            loadPaths: [
                 "node_modules/foundation-sites/scss/",
                 "node_modules/motion-ui/src/",
             ],
             importer: createImporter(),
         }),
         // Copy in the static external libraries
-        copy.default({assets: {
-            from: [
-                "node_modules/jquery/dist/jquery.min.js",
-                "node_modules/what-input/dist/what-input.min.js",
-            ],
-            to: [ "js/vendor" ]
-        }})
+        // https://github.com/LinbuduLab/esbuild-plugins/issues/105
+        copy({ assets: {
+            from : "node_modules/jquery/dist/jquery.min.js",
+            to   : "js/vendor"
+        }}),
+        copy({ assets: {
+            from : "node_modules/what-input/dist/what-input.min.js",
+            to   : "js/vendor"
+        }}),
     ],
 });
